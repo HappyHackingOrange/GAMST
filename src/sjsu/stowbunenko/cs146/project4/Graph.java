66a7ea18a16a5663570bda61c298f8d5c50204f4
Manavivian <h.vivian92@gmail.com>
2017-12-05 21:23:04 -0800

    Changed according to fit HashMap, but did not change the toString method not the tests

--- src/sjsu/stowbunenko/cs146/project4/Graph.java
+++ src/sjsu/stowbunenko/cs146/project4/Graph.java
@@ -5,7 +5,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Undirected and weighted graph represented by adjacency list.
@@ -16,15 +21,14 @@ import java.util.*;
 public class Graph {
 
 	// Instance variables
-	ArrayList<LinkedList<Pair<Vertex, Double>>> adjacencyList;
-	ArrayList<Pair<Integer, Double>> queue;
+	HashMap<Vertex, LinkedList<Pair<Vertex,Double>>> adjacencyList;
 	private int edge;
 	private int vertices;
 
 	public Graph() {
-		adjacencyList = new ArrayList<LinkedList<Pair<Vertex, Double>>>(10001);
+		adjacencyList = new HashMap<Vertex, LinkedList<Pair<Vertex,Double>>>(10001);
 		for (int x = 0; x < 10001; x++) {
-			adjacencyList.add(x, new LinkedList<Pair<Vertex, Double>>());
+			adjacencyList.put(new Vertex(), new LinkedList<Pair<Vertex, Double>>());
 		}
 		vertices = 0;
 		edge = 0;
@@ -41,12 +45,18 @@ public class Graph {
 	 *            the weight on the edge between vertices u and v
 	 */
 	public void addEdge(int source, int destination, double weight) {
-		Vertex theSource = new Vertex(source,destination);
-		Vertex theDestination = new Vertex(destination,source);
+		Map<Vertex, LinkedList<Pair<Vertex,Double>>> map = adjacencyList;
+		Vertex theSource = new Vertex(source);
+		Vertex theDestination = new Vertex(destination);
+		
+		LinkedList<Pair<Vertex, Double>> neighbor = map.get(theSource);
+		LinkedList<Pair<Vertex, Double>> neighborReturn = map.get(theDestination);
+		
 		Pair<Vertex, Double> pair = new Pair<Vertex, Double>(theSource, weight);
 		Pair<Vertex, Double> reversePair = new Pair<Vertex, Double>(theDestination, weight);
-		adjacencyList.get(source).add(pair);
-		adjacencyList.get(destination).add(reversePair);
+		neighbor.add(reversePair);
+		neighborReturn.add(pair);
+
 	}
 
 	/**
@@ -54,9 +64,10 @@ public class Graph {
 	 * 
 	 * @return an ArrayList of neighbors from the source
 	 */
-	public ArrayList<Pair<Vertex, Double>> findNeighbors(int source) {
+	public ArrayList<Pair<Vertex, Double>> findNeighbors(Vertex source) {
+		Map<Vertex, LinkedList<Pair<Vertex,Double>>> map = adjacencyList;
 		ArrayList<Pair<Vertex, Double>> neighbors = new ArrayList<Pair<Vertex, Double>>();
-		for (Pair<Vertex, Double> current : adjacencyList.get(source)) {
+		for (Pair<Vertex, Double> current : map.get(source)) {
 			neighbors.add(current);
 		}
 		return neighbors;
@@ -102,21 +113,17 @@ public class Graph {
 	 *
 	 * Prim's algorithm based on what the Professor has given according to the
 	 * slides
+	 * This will return the ROOT vertex
 	 * 
 	 * @param graph
 	 * @param weight
 	 * @param vertex
 	 */
 
-	public Graph Prim(double weight, int vertex) {
+	public Vertex Prim(double weight, int vertex) {
 		// Queue of vertices
-		Graph mingraph = new Graph();
-		int current = vertex;
-		while (!queue.isEmpty()) {
-			current = ExtractMinimumEdge(current).x.destination;
-
-		}
-		return mingraph;
+		
+		
 	}
 
 
@@ -153,14 +160,28 @@ public class Graph {
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
+/*		Map<Vertex, LinkedList<Pair<Vertex,Double>>> map = adjacencyList;
+		Set set = adjacencyList.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()) {
+			Map.Entry entry = (Map.Entry)i.next();
+			Vertex current = (Vertex) entry.getKey();
+			stringBuilder.append(current.key + ": ");
+			for(Pair<Vertex,Double> pair: (LinkedList) entry.getValue()) {
+				
+			}
+		}*/
+		
+		
+		/*
 		for (int i = 0; i < adjacencyList.size(); i++) {
 			if (adjacencyList.get(i).peek() != null) {
 				stringBuilder.append(String.format("%d: ", i));
 				for (Pair<Vertex, Double> pair : adjacencyList.get(i))
-					stringBuilder.append(String.format("(%d, %.2f) ", pair.x.destination, pair.y));
+					stringBuilder.append(String.format("(%d, %.2f) ", pair.x, pair.y));
 				stringBuilder.append("\n");
 			}
-		}
+		}*/
 		return stringBuilder.toString();
 	}
 
--- src/sjsu/stowbunenko/cs146/project4/Vertex.java
+++ src/sjsu/stowbunenko/cs146/project4/Vertex.java
@@ -3,8 +3,6 @@ package sjsu.stowbunenko.cs146.project4;
 public class Vertex {
 	
 	double key;
-	int source;
-	int destination;
 	Vertex head;
 	Vertex child;
 	/**
@@ -12,12 +10,18 @@ public class Vertex {
 	 * @param source
 	 * @param destination
 	 */
-	public Vertex(int source, int destination) {
-		this.source = source;
-		this.destination = destination;
+	public Vertex() {
 		key = Double.MAX_VALUE;
 		head = null;
 		child = null;
+	}
+	
+	/**
+	 * This will assign the value of the vertex_number given by the txt files
+	 * @param head
+	 */
+	public Vertex(int head) {
+		key = head;
 	}
 	
 }
